a22a405213a1f84acbafaa12c4d4456d0723a69b
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2019-01-21 21:05:28 +0530

    Message Receiving Service

---
+++ newLanChat/receiver.py
@@ -0,0 +1,16 @@
+from standards import BUFFER_SIZE
+from socket import socket
+
+
+class Receiver(threading.Thread):
+	def __init__(self, socket_object):
+		threading.Thread.__init__(self)
+		self.socket_object = socket_object
+
+	def run(self):
+		while True:
+			try:
+				msg = self.socket_object.recv(BUFFER_SIZE)
+				print msg
+			except socket.error:
+				print "Error receiving messages."
